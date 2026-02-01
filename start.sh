b6bb844f6451f94ca70d0bee90342f15a3d5b83b
Arunkumar S <sivaarunaj@gmail.com>
2026-02-01 16:40:51 +0530

    fix: remove the start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# AnchorFX Backend - Railway Start Script
-
-echo "Installing dependencies for the backend of the application"
-cd backend && npm install
-
-echo "Starting AnchorFX backend server..."
-node server.js
